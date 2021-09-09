4b01ac7b55eaae16d0610dad4b9903eaab0198d3
hectorjog <81263181+hectorjog@users.noreply.github.com>
2021-09-08 22:51:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static('dist/'));
+app.use(express.static('dist/pruebascloudrun'));
 app.get('/', function (req, res,next) {
 res.redirect('/'); 
 });
